39d3a84ecdecdc2da24557d5401187e64bf37de6
ValentinaRippe <lauravalentina200102@gmail.com>
2022-09-21 22:13:06 -0500

    Change route report recipe

--- src/routes/recipeReports.routes.js
+++ src/routes/recipeReports.routes.js
@@ -12,7 +12,7 @@ router.get('/report-recipe/:id', authAdmin, getActiveReports)
 router.post('/report-recipe', createReport)
 router.put('/report-recipe/:id', authAdmin, rejectReport)
 router.delete('/report-recipe/', authAdmin, deleteAllReports)
-router.delete('/report/:id', authAdmin, deleteByIdReports)
+router.delete('/report-recipe/:id', authAdmin, deleteByIdReports)
 
 
 export default router
