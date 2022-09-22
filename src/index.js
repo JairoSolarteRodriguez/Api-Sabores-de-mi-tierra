2957ed580cc953252628276eac4fbdda893cde44
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-09-21 21:45:01 -0500

    force remove

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import app from "./app.js"
 import { sequelize } from "./db/db.js"
 
-import './models/Users.js'
-import './models/UsersProfile.js'
-import './models/UsersReported.js'
-import './models/Categories.js'
-import './models/Dificults.js'
-import './models/Ingredients.js'
-import './models/Prices.js'
-import './models/Tools.js'
-import './models/Measures.js'
-import './models/Steps.js'
-import './models/Recipes.js'
-import './models/StepTools.js'
-import './models/StepIngredients.js'
-import './models/StepMeasures.js'
-import './models/RecipeCategories.js'
-import './models/FavoriteRecipes.js'
-import './models/StepRecipes.js'
-import './models/Stars.js'
-import './models/AppFeedbacks.js'
-import './models/Comments.js'
-import './models/CommentsRecipes.js'
-import './models/RecipesReported.js'
+// import './models/Users.js'
+// import './models/UsersProfile.js'
+// import './models/UsersReported.js'
+// import './models/Categories.js'
+// import './models/Dificults.js'
+// import './models/Ingredients.js'
+// import './models/Prices.js'
+// import './models/Tools.js'
+// import './models/Measures.js'
+// import './models/Steps.js'
+// import './models/Recipes.js'
+// import './models/StepTools.js'
+// import './models/StepIngredients.js'
+// import './models/StepMeasures.js'
+// import './models/RecipeCategories.js'
+// import './models/FavoriteRecipes.js'
+// import './models/StepRecipes.js'
+// import './models/Stars.js'
+// import './models/AppFeedbacks.js'
+// import './models/Comments.js'
+// import './models/CommentsRecipes.js'
+// import './models/RecipesReported.js'
 
 const PORT = process.env.PORT || 3000
 
 async function main() {
   try {
-    await sequelize.sync({ force: true })
-    // await sequelize.sync()
+    // await sequelize.sync({ force: true })
+    await sequelize.sync()
     console.log("Connection has been established successfully.")
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
