67bde49f5f1aa61209199862eda4c6d3efb806fe
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-09-22 14:53:40 -0500

    fixed authAdmin middleware

--- src/middlewares/authAdmin.js
+++ src/middlewares/authAdmin.js
@@ -7,14 +7,17 @@ const { REFRESH_TOKEN_SECRET } = process.env
 
 export const authAdmin = async (req, res, next) => {
   try {
-    const authorizationToken = req.header("Authorization")
 
-    if(!authorizationToken) return res.status(401).send({message: `Autorización invalida`})
+    const { authorization } = req.headers
+
+    if(!authorization) return res.status(401).send({message: `Autorización invalida`})
     
-    const userToken = jwt.verify(authorizationToken, REFRESH_TOKEN_SECRET)
+    const userToken = jwt.verify(authorization, REFRESH_TOKEN_SECRET)
     if(!userToken) return res.status(400).send({ message: `Autorización invalida` })
     
-    const user = await User.findOne({ where: {userId: userToken.id }})
+    const user = await User.findOne({ where: { userId: userToken.id } })
+
+    if(!user) return res.status(400).send({ message: `Autorización invalida` })
 
     if(user.userIsAdmin || user.userIsStaff){
       next()
